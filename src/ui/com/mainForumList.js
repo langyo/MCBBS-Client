ded91df515026ce5c388dc2a192bf84e84d2d1d9
langyo <m13776491897@163.com>
2018-10-27 18:15:13 +0800

    .

--- src/ui/com/mainForumList.js
+++ src/ui/com/mainForumList.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
 import List from "@material-ui/core/List";
@@ -11,26 +12,38 @@ import Avatar from "@material-ui/core/Avatar";
 import { res } from "../../lib/resources";
 
 const styles = theme => ({
-  list:{
-    marginTop:theme.spacing.unit * 2
+  list: {
+    marginTop: theme.spacing.unit * 2
   },
-  subHeader:{
-    backgroundColor:theme.palette.background.paper
+  subHeader: {
+    backgroundColor: theme.palette.background.paper
   }
 });
 
-class MainPageForumList extends React.Component{
-  render(){
-    return({
+class MainPageForumList extends React.Component {
+  render() {
+    return (
       <React.Component>
         <List className={classes.list}>
-          {
-            res.mainPage.map(({ forumGroupName, forums }) => (
-              <Fragment
-            ));
-          }
+          {res.mainPage.map(({ forumGroupName, forums }) => (
+            <Fragment key={forumGroupName}>
+              <ListSubheader className={classes.subHeader}>
+                {forumGroupName}
+              </ListSubheader>
+              {forums.map(({ name, info, avatar }) => (
+                <ListItem button onClick={null}>
+                  <Avatar src={avatar} />
+                  <ListItemText primary={name} secondary={info} />
+                </ListItem>
+              ))}
+            </Fragment>
+          ))}
         </List>
       </React.Component>
-    });
+    );
   }
 }
+
+MainPageForumList.propTypes = {
+  classes: PropTypes.object.isRequired
+};
