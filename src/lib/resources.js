1b7837d2a021e0870cfb6c71f3be508fdeff9417
langyo <m13776491897@163.com>
2018-11-04 01:09:59 +0800

    .

--- src/lib/resources.js
+++ src/lib/resources.js
@@ -189,6 +189,7 @@ let res = {
     */
   },
 
+  // 勋章
   medals: {
     // [map:勋章 ID]
     /*
@@ -198,6 +199,198 @@ let res = {
         info - 勋章说明
         achieveWay - 获得方式
     */
+  },
+
+  // 道具
+  tools: {
+    // [map:道具标识名]
+    /*
+      元素列表：
+        name - 道具名
+        info - 道具说明
+        prize - 价格
+        integralType - 用于购买道具的积分类型
+        weight - 质量
+    */
+  },
+
+  // 本地账户
+  accounts: {
+    // 当前正在使用的已登入账户 ID
+    nowUsing: 0,
+    loggedAccount: {
+      // [map:登入账户 ID]
+      /*
+        元素列表：
+        notifications[] - 提醒列表：
+          noticer - 提醒者 ID；如果为 0 则为系统通知
+          content - 提醒内容
+          href - 指向的链接
+          time - 提醒时间
+        messages{} - 私信列表：
+          [map:聊天对象 ID]
+          type - 消息来源类型，只有 'send' | 'receive' 两种类型
+          content - 消息内容，这里直接存储 HTML 代码（未来的版本中会试图将下载的 HTML 代码转换为 UUB 再进行储存）
+          time - 消息创建时间
+        friends[] - 朋友 ID 列表
+        shortMessages{} - 打招呼列表：
+          [map:聊天对象 ID]
+          content - 打招呼内容
+          emoji - 打招呼表情
+          time - 打招呼时间
+          history[] - 历史记录
+            content - 打招呼内容
+            emoji - 打招呼表情
+            time - 打招呼时间
+        forumManageMessages[] - 论坛管理消息列表：
+          handler - 操作者
+          integralMessage - 操作信息，这部分信息是没过滤过的积分提示信息
+          reason - 理由
+          time - 操作时间
+        imtegralHisTory{} - 积分操作记录
+          [map:积分类型]
+          type - 操作理由的分类类型
+          count - 变更积分数量
+          time - 操作时间
+        packbag - 背包信息
+          topWeight - 最大的背包可容纳质量大小
+          nowWeight - 当前的背包已容纳质量大小
+          tools{} - 具体明细
+            [map:道具标识名]
+            count - 数量
+        favoriteThreads[] - 收藏帖子列表
+          href - 帖子 ID
+          time - 收藏时间
+          reason - 收藏理由
+      */
+    }
+  },
+
+  // 本地存贮的设置数据
+  local: {
+    // 日志记录
+    log: [
+      /*
+        元素列表：
+          type - 日志类型，只有 'warn' | 'info' | 'error' | 'log' 这四种类型
+          content - 内容
+          time - 记录时间
+          from - 来源，可以是客户端本身，也可以是客户端下的其中一个虚拟浏览器，或是挂载在客户端上的插件
+      */
+    ],
+
+    // 浏览记录
+    history: {
+      // 页面浏览历史
+      browseHistory: [
+        /*
+          元素列表：
+            href - 链接，这里必须给出完整的 URL，因为可能还会访问外部网页
+            title - 页面标题
+            time - 浏览时间
+        */
+      ],
+
+      // 账户登入历史
+      accountHistory: [
+        /*
+          元素列表：
+            name - 账户名
+            uid - 账户 ID
+        */
+      ],
+
+      // 账户登入使用的 Cookie 凭据，存储的是 CookieJar 对象的 json 文本表达
+      accountCookieStorage: {
+        // [map:账户 ID]
+      },
+
+      // 插件启动记录
+      extensionLaunchHistory: [
+        /*
+          元素列表：
+            uuid - 插件 UUID
+            time - 启动时间
+        */
+      ]
+    },
+
+    // 插件
+    extensions: {
+      // [map:插件 UUID]
+      /*
+        元素列表：
+          name - 插件名称，可以是中文
+          use - 是否使用，只有 'true' | 'false' 这两个类型
+          codes - 插件代码的字符串，在启用时客户端会将其作为 JavaScript 代码隔离在沙箱中执行
+          author - 作者 ID
+          sourceLibrary[] - 依赖的其它插件 UUID 列表
+      */
+    },
+
+    // 资源数据
+    sources: {
+      // [map:资源文件的 URL 的特征码]
+      /*
+        元素列表：
+          size - 大小
+          localStorageNodeId - 对应于本地存储的节点 ID，用于同步
+          content - 缓存的数据
+          time - 数据的创建时间
+          url - 数据所在位置的完整 URL
+      */
+    },
+
+    // 虚拟浏览器设置
+    browserSettings: {
+      // User-Agent 设置
+      userAgent: ""
+    },
+
+    // 客户端个性化设置
+    clientSettings: {
+      // 主题
+      theme: ""
+    },
+
+    // 临时储存的草稿帖子
+    tempSaveThreads: [
+      /*
+        元素列表：
+          time - 保存时间
+          title - 标题
+          content - 内容
+          tags{} - 帖子属性：
+            awardInfo{} - 帖子回复奖励信息：
+              integralType - 奖励积分类型
+              integralCount - 当前帖子剩余的可用奖励积分总量
+              everyoneAllocateIntegralCount - 每个人能分配的积分量
+              everyoneAllocateIntegralTimes - 每个人最多能被分配积分的次数（也就是最大的能获得奖励的回复次数）
+              everyoneAllocateIntegralChance - 每个人能够中奖的概率
+            tags[] - 帖子被加上的分类标签（这种标签是论坛自带的分类标签，由发帖者自行决定）；默认无分类标签
+      */
+    ],
+
+    // 临时储存的草稿回复
+    tempSaveReplys: [
+      /*
+        元素列表：
+          time - 保存时间
+          content - 内容
+          replyToThread - 回复到的主题 ID
+          replyToPost - 回复到的楼层编码；默认为 1（即楼主层）
+      */
+    ],
+
+    // 储存的评分理由
+    rateReasons: [
+      /* 字符串数组 */
+    ],
+
+    // 储存的举报理由
+    reportReasions: [
+      /* 字符串数组 */
+    ]
   }
 };
 
