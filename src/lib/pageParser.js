937030075c56b20b9deaa67b2e9b330903e65158
langyo <m13776491897@163.com>
2018-11-06 06:52:50 +0800

    .

--- src/lib/pageParser.js
+++ src/lib/pageParser.js
@@ -2,3 +2,13 @@
  *@author langyo
  *@file 页面解析支持，对不同的页面进行解析，并向 resources 与 localStorage 写入正确的数据结构
  */
+
+function querySelection(doc, str) {}
+
+function DOMElements(elements) {}
+
+DOMElements.prototype = {
+  getIds: function() {},
+  getClasses: function() {},
+  getListeners: function(listenerType) {}
+};
---
+++ src/lib/pageParser.md
@@ -0,0 +1,17 @@
+# pageParser 模块
+
+pageParser 提供了页面解析支持，对不同的页面进行解析，并向 resources 与 localStorage 写入正确的数据结构。
+
+# 函数说明
+
+### querySelection(doc, str)
+
+### DOMElements (构造函数)
+
+#### 方法列表
+
+##### DOMElements.getIds()
+
+##### DOMElements.getClasses()
+
+##### DOMElements.getListeners(listenerType)
