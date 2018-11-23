0df3383949fffa00a48ecb2ee5b756f7f2395975
langyo <m13776491897@163.com>
2018-11-23 16:14:59 +0800

    .

--- plugin/langyo/monkey/main.js
+++ plugin/langyo/monkey/main.js
@@ -1,17 +1,23 @@
 function Monkey(document){
     this.document = document;
 
-    // 获取 ID 列表
-    this.id = (function dfs(node){
-        let ret = [];
-        for(let i of node.childNodes){
-            if(i.id) ret.push(i.id);
-            if(i.nodeType === 1){
-                let n = dfs(i);
-                for(let j of i) ret.push(j);
+    // 获取页面的所有 ID
+    let idList = [];
+    function dfs(node){
+        if(node && node.nodeType === document.ELEMENT_NODE){
+            if(node.id) idList(node.id);
+        }
+        let childNodes = node.childNodes;
+        for(let i = 0; i < childNodes.length; ++i){
+            let item = childNodes[i];
+            if(item.nodeType === 1){
+                dfs(item);
             }
         }
-    })(document.body);
+    }
+    dfs(document.body);
+
+    this.idList = idList;
 }
 
 Monkey.prototype.bind = function(selector, event, callback){
@@ -19,5 +25,33 @@ Monkey.prototype.bind = function(selector, event, callback){
 }
 
 Monkey.prototype.watch = function(selector, callback){
-    
+
+}
+
+Monkey.prototype.send = function(selector, event, args){
+
+}
+
+Monkey.prototype.getNodes = function(selector){
+
+}
+
+Monkey.prototype.reviewIDs = function(){
+    // 获取页面的所有 ID
+    let idList = [];
+    function dfs(node){
+        if(node && node.nodeType === document.ELEMENT_NODE){
+            if(node.id) idList(node.id);
+        }
+        let childNodes = node.childNodes;
+        for(let i = 0; i < childNodes.length; ++i){
+            let item = childNodes[i];
+            if(item.nodeType === 1){
+                dfs(item);
+            }
+        }
+    }
+    dfs(document.body);
+
+    this.idList = idList;
 }
