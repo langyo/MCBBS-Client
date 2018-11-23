f404219d26770ccf0ab853381c2469d0ab8e49da
langyo <m13776491897@163.com>
2018-11-23 12:05:46 +0800

    .

--- plugin/langyo/monkey/main.js
+++ plugin/langyo/monkey/main.js
@@ -1,28 +1,23 @@
-let sliceByChar = (char, str) => {
-    let arr = [], pos = 0;
-    let getOuterPos = (char, pos, str) =>{
-        let left = str.indexOf(char, 0);
-        let right = str.indexOf(char, pos);
-        if(left < pos && pos < right) return right;
-        return 0;
-    };
-    let getSearchBeginPos = (pos, str) => ['"', "'", "/"].map(n, getOuterPos(n, pos, str)).reduce((past, present) => past > present ? past : present);
-    while(pos = str.indexOf(char, getSearchBeginPos(str.indexOf(char), str)), pos >= 0){
-        arr.push(str.slice(0, pos));
-        str = str.slice(pos);
-        pos = 0;
-    }
-    arr.push(str);
-    return arr;
-};
+function Monkey(document){
+    this.document = document;
 
-let sliceByNewLine = str => sliceByChar("\n", str);
-let sliceBySpace = str => sliceByChar(" ", str);
+    // 获取 ID 列表
+    this.id = (function dfs(node){
+        let ret = [];
+        for(let i of node.childNodes){
+            if(i.id) ret.push(i.id);
+            if(i.nodeType === 1){
+                let n = dfs(i);
+                for(let j of i) ret.push(j);
+            }
+        }
+    })(document.body);
+}
 
-let parseCommandLine = (commands, doms) =>{
-    // 待编辑
-};
+Monkey.prototype.bind = function(selector, event, callback){
 
-let parse = (str, dom) => sliceByNewLine(str).forEach(n => {
-    // 待编辑
-});
+}
+
+Monkey.prototype.watch = function(selector, callback){
+    
+}
