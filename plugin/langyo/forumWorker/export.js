a58cacc0f126982deda01421576d24f780fb1fdb
langyo <m13776491897@163.com>
2018-11-24 18:08:55 +0800

    总算是开始写解析器了

--- plugin/langyo/forumWorker/export.js
+++ plugin/langyo/forumWorker/export.js
@@ -50,6 +50,12 @@ let parser = {
 
     },
     watchThread:(document) =>{
-        
+        let thread = {};
+        // 主题作者，第一页时为一楼作者，其它页时楼主在主题头部位置
+        /* document.querySelectorAll('#tath > a:nth-child(2)')[0].innerHTML */
+        // 查看量
+        thread.watchCount = document.querySelectorAll('#postlist > table:nth-child(1) > tbody > tr > td.pls.ptn.pbn > div > span:nth-child(2)')[0].innerHTML;
+        // 回复量
+        thread.replyCount = document.querySelectorAll('#postlist > table:nth-child(1) > tbody > tr > td.pls.ptn.pbn > div > span:nth-child(5)')[0].innerHTML;
     }
 };
