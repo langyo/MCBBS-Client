d9cb7fa39a5adaa97a2f83c109582a9aa3f75a0b
langyo <m13776491897@163.com>
2018-11-24 10:11:54 +0800

    注意，此部分代码将会再次进行大的改动

--- plugin/langyo/monkey/main.js
+++ plugin/langyo/monkey/main.js
@@ -1,42 +1,39 @@
-function getIDs(document){
-    // 获取页面的所有 ID
-    let idList = [];
-    function dfs(node){
-        if(node && node.nodeType === document.ELEMENT_NODE){
-            if(node.id) idList.push(node.id);
-        }
-        let childNodes = node.childNodes;
-        for(let item of childNodes){
-            if(item.nodeType === 1){
-                dfs(item);
-            }
+function Monkey(document){
+    this.document = document.body;
+    this.actions = {};
+}
+
+Monkey.prototype.send = function(selector, event){
+    eval(document.querySelectorAll(selector).getAttribute(event));
+};
+
+Monkey.prototype.filterNode = function(selectorList){
+    let ret = {};
+    for(let i of selectorList){
+        ret[i] = this.document.querySelectorAll(i);
+    }
+    return ret;
+};
+
+Monkey.prototype.defineActions = function(name, actions){
+    // 无效动作检查
+    for(let i of actions){
+        switch(i.type){
+            case 'send':
+            
+            case 'filterNode':
+
+            case 'runActions':
+
+            default:
+                throw new Error('未知动作');
         }
     }
-    dfs(document);
-    return idList;
+
+    this.actions[name] = actions;
 }
 
-function Monkey(document){
-    this.document = document;
-    this.idList = getIDs(document.body);
-}
-
-Monkey.prototype.bind = function(selector, event, callback){
-
-}
-
-Monkey.prototype.watch = function(selector, callback){
-
-}
-
-Monkey.prototype.send = function(selector, event, args){
-
-}
-
-Monkey.prototype.getNodes = function(selector){
-
-}
-
-Monkey.prototype.reviewIDs = function(){
-    this.idList = getIDs(this.document.body);
+Monkey.prototype.runActions = function(actions){
+    let nextInput;
+    for(let i of actions) nextInput = this[this.actions[i].type].apply(this.actions[i].data.push(nextInput));
 }
