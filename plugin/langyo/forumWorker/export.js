1f358f863fe46f4d8cff56f7962cb550a003e978
langyo <m13776491897@163.com>
2018-11-24 12:18:22 +0800

    有时候，简单一点更好

---
+++ plugin/langyo/forumWorker/export.js
@@ -0,0 +1,55 @@
+let databaseLinker = (path) =>{
+
+};
+
+let urlLinker = (url) =>{
+
+};
+
+let parser = {
+    chatting:(document) =>{
+
+    },
+    chattingList:(documnet) =>{
+
+    },
+    forum:(document) =>{
+
+    },
+    login:(document) =>{
+
+    },
+    newThread:(document) =>{
+
+    },
+    notices:(document) =>{
+
+    },
+    recentNews:(document) =>{
+
+    },
+    register:(document) =>{
+
+    },
+    reply:(document) =>{
+
+    },
+    report:(document) =>{
+
+    },
+    review:(document) =>{
+
+    },
+    search:(document) =>{
+
+    },
+    userCenter:(document) =>{
+
+    },
+    userInfo:(document) =>{
+
+    },
+    watchThread:(document) =>{
+        
+    }
+};
--- plugin/langyo/monkey/document.md
+++
@@ -1,8 +0,0 @@
-# monkey
-
-## 示例
-
-    import { Monkey } from 'plugin/langyo/monkey/main.js';
-    let monkey = new Monkey(mainPage.webContent.hostContent);
-    monkey.bind("#/post[0-9]+/", "onClick", fn);
-    monkey.watch("#/post[0-9]+/", fn2);
