3c390eac984426177652d0a2f6d62e236293e053
langyo <m13776491897@163.com>
2018-11-27 06:56:10 +0800

    加了个try，好歹让程序不会立即崩

--- plugin/langyo/forumWorker/pageParser/watchThread.js
+++ plugin/langyo/forumWorker/pageParser/watchThread.js
@@ -2,42 +2,42 @@ export default (document) =>{
     let thread = {};
     let posts = {};
 
-    // 帖子回复列表解析
-    let postList = document.querySelectorAll('#postlist > div');
-    if(!Array.isArray(thread.posts)) thread.posts = [];
-    for(let i = 0; i < postList.length; ++i){
-        let match;
-        if(match = /^post_([0-9]+)$/.exec(postList[i].id) ){
-            thread.posts.push(match[1]);
-            posts[postList[i].id] = {};
-            // 内容
-            posts[postList[i].id].content = postList[i].querySelectorAll('table > tbody > tr')[0].querySelectorAll('td.plc > div.pct > div.pcb > div.t_fsz')[0].innerHTML;
-            // 作者
-            posts[postList[i].id].author = postList[i].querySelectorAll('table > tbody > tr')[0].querySelectorAll('td.pls > div > div.pi > div > a')[0].innerHTML
-        }
-    }
-
-    // 标题解析
-    thread.title = document.querySelectorAll('#thread_subject')[0].innerHTML;
-
-    // 帖子类型解析
-    thread.type = /typeid=([0-9]+)/.exec(document.querySelectorAll('#postlist > table')[0].querySelectorAll('tbody > tr > td.plc.ptm.pbn.vwthd > h1 > a')[0].getAttribute('href'))[1];
-
-    // 帖子状态解析
-    if(typeof thread.states !== 'object') thread.states = {};
-    if(document.querySelectorAll('#postlist > table')[0].querySelectorAll('tbody > tr > td.plc.ptm.pbn.vwthd > span > img').length > 0){
-        let n = document.querySelectorAll('#postlist > table')[0].querySelectorAll('tbody > tr > td.plc.ptm.pbn.vwthd > span > img')
-        for(let i of n){
-            n[i] = n[i].getAttribute('src');
-        }
-        for(let i of n){
-            // 是否被关闭
-            if(/image\/locked\.gif/.test(n[i])) thread.states.closed = true;
-        }
-    }
-
-    // 主题头部解析
     try{
+        // 帖子回复列表解析
+        let postList = document.querySelectorAll('#postlist > div');
+        if(!Array.isArray(thread.posts)) thread.posts = [];
+        for(let i = 0; i < postList.length; ++i){
+            let match;
+            if(match = /^post_([0-9]+)$/.exec(postList[i].id) ){
+                thread.posts.push(match[1]);
+                posts[postList[i].id] = {};
+                // 内容
+                posts[postList[i].id].content = postList[i].querySelectorAll('table > tbody > tr')[0].querySelectorAll('td.plc > div.pct > div.pcb > div.t_fsz')[0].innerHTML;
+                // 作者
+                posts[postList[i].id].author = postList[i].querySelectorAll('table > tbody > tr')[0].querySelectorAll('td.pls > div > div.pi > div > a')[0].innerHTML
+            }
+        }
+
+        // 标题解析
+        thread.title = document.querySelectorAll('#thread_subject')[0].innerHTML;
+
+        // 帖子类型解析
+        thread.type = /typeid=([0-9]+)/.exec(document.querySelectorAll('#postlist > table')[0].querySelectorAll('tbody > tr > td.plc.ptm.pbn.vwthd > h1 > a')[0].getAttribute('href'))[1];
+
+        // 帖子状态解析
+        if(typeof thread.states !== 'object') thread.states = {};
+        if(document.querySelectorAll('#postlist > table')[0].querySelectorAll('tbody > tr > td.plc.ptm.pbn.vwthd > span > img').length > 0){
+            let n = document.querySelectorAll('#postlist > table')[0].querySelectorAll('tbody > tr > td.plc.ptm.pbn.vwthd > span > img')
+            for(let i of n){
+                n[i] = n[i].getAttribute('src');
+            }
+            for(let i of n){
+                // 是否被关闭
+                if(/image\/locked\.gif/.test(n[i])) thread.states.closed = true;
+            }
+        }
+
+        // 主题头部解析
         if(document.querySelectorAll('#tath > a').length > 0){
             // 其它页时为一楼作者
             document.querySelectorAll('#tath > a')[1].innerHTML ? thread.author = document.querySelectorAll('#tath > a')[1].innerHTML : undefined;
