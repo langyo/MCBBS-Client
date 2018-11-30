00533548e6bdaa3346c72621583f68d5316ccbf4
langyo <m13776491897@163.com>
2018-11-30 12:14:16 +0800

    .

--- native/electron/app/mainWindow.js
+++ native/electron/app/mainWindow.js
@@ -1,32 +1,3 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-
-import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import ExtensionIcon from '@material-ui/icons/Extension';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import InfoIcon from '@material-ui/icons/Info';
-import CloseIcon from '@material-ui/icons/Close';
-
 const events = window.require('events');
 const path = window.require('path');
 const fs = window.require('fs');
@@ -35,195 +6,96 @@ const electron = window.require('electron');
 const { ipcRenderer, shell } = electron;
 const { dialog } = electron.remote;
 
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ExtensionIcon from '@material-ui/icons/Extension';
+
 const drawerWidth = 240;
-
-// const theme = createMuiTheme({
-//     palette: {
-//         primary: '#0097a7',
-//         secondary: '#039be5',
-//     }
-// });
-
-// function withRoot(Component) {
-//     function WithRoot(props) {
-//       return (
-//         <MuiThemeProvider theme={theme}>
-//           <CssBaseline />
-//           <Component {...props} />
-//         </MuiThemeProvider>
-//       );
-//     }
-  
-//     return WithRoot;
-//   }
 
 const styles = theme => ({
     root: {
         display: 'flex',
-        primary: {
-            main: '#0097a7'
-        },
-        secondary: {
-            main: '#039be5'
-        },
     },
     appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
         width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginLeft: 12,
-        marginRight: 36,
-    },
-    hide: {
-        display: 'none',
+        marginLeft: drawerWidth,
     },
     drawer: {
         width: drawerWidth,
         flexShrink: 0,
-        whiteSpace: 'nowrap',
     },
-    drawerOpen: {
+    drawerPaper: {
         width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
     },
-    drawerClose: {
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        width: theme.spacing.unit * 7 + 1,
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing.unit * 9 + 1,
-        },
-    },
-    toolbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: '0 8px',
-        ...theme.mixins.toolbar,
-    },
+    toolbar: theme.mixins.toolbar,
     content: {
         flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
         padding: theme.spacing.unit * 3,
-    },
-    grow: {
-        flexGrow: 1,
     },
 });
 
-class MainWindow extends React.Component {
-    constructor(props) {
-        super(props);
+function MainWindow(props) {
+    const { classes } = props;
 
-        this.state = {
-            // open: false
-        };
-    }
-
-    // handleDrawerOpen() {
-    //     this.setState({ open: true });
-    // }
-
-    // handleDrawerClose() {
-    //     this.setState({ open: false });
-    // }
-
-    // handleDialogClose() {
-
-    // }
-
-    render() {
-        const { classes, theme } = this.props;
-
-        // theme.palette.augmentColor(primary);
-        // theme.palette.augmentColor(secondary);
-
-        return (
-            <div className={classes.root}>
-                <CssBaseline />
-                <AppBar
-                    position="fixed"
-                    className={classNames(classes.appBar, {
-                        [classes.appBarShift]: this.state.open,
-                    })}
-                >
-                    <Toolbar disableGutters={!this.state.open}>
-                        <IconButton />
-                        <Typography variant="h6" color="inherit">
-                            Client
-                        </Typography>
-                        <div className={classes.grow} />
-                        <div>
-                            <IconButton color="inherit">
-                                <CloseIcon />
-                            </IconButton>
-                        </div>
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    variant="permanent"
-                    className={classNames(classes.drawer, {
-                        [classes.drawerOpen]: this.state.open,
-                        [classes.drawerClose]: !this.state.open,
-                    })}
-                    classes={{
-                        paper: classNames({
-                            [classes.drawerOpen]: this.state.open,
-                            [classes.drawerClose]: !this.state.open,
-                        }),
-                    }}
-                    open={ /*this.state.open*/ true }
-                >
-                    <div className={classes.toolbar}>
-                        <IconButton onClick={this.handleDrawerClose}>
-                            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-                        </IconButton>
-                    </div>
-                    <Divider />
-                    <List>
-                        <ListItem button>
-                            <ListItemIcon> <InsertDriveFileIcon /></ListItemIcon>
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon> <AccountCircleIcon /></ListItemIcon>
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon> <ExtensionIcon /></ListItemIcon>
-                        </ListItem>
-                    </List>
-                    <Divider />
-                </Drawer>
-                <main className={classes.content}>
-                    <div className={classes.toolbar} />
-                    {/* 实体内容 */}
-                </main>
-            </div>
-        );
-    }
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="h6" color="inherit" noWrap>
+                        Client Test UI
+          </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+                anchor="left"
+            >
+                <div className={classes.toolbar} />
+                <Divider />
+                <List>
+                    <ListItem button>
+                        <ListItemIcon> <InsertDriveFileIcon /></ListItemIcon>
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon> <AccountCircleIcon /></ListItemIcon>
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon> <ExtensionIcon /></ListItemIcon>
+                    </ListItem>
+                </List>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Typography paragraph>
+                    test
+                </Typography>
+            </main>
+        </div>
+    );
 }
 
 MainWindow.propTypes = {
     classes: PropTypes.object.isRequired,
-    theme: PropTypes.object.isRequired,
 };
 
-// export default withRoot(withStyles(styles, { withTheme: true })(MainWindow));
-
-export default withStyles(styles, { withTheme: true })(MainWindow)
+export default withStyles(styles, { withTheme: true })(MainWindow);
