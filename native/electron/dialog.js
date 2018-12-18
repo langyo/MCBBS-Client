15c2d1612bb9b0d842af185a76dbaf99b7b2d950
langyo <langyo.china@gmail.com>
2018-12-18 19:23:52 +0800

    试验成功，即将改造 scripts 文件夹，转入以库的形式引用平台无关代码

--- native/electron/dialog.js
+++ native/electron/dialog.js
@@ -63,7 +63,7 @@ import InfoIcon from '@material-ui/icons/Info';
 import AddIcon from '@material-ui/icons/Add';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 
-import HeadImageRender from './scripts/viewManager/utils/_headImage';
+import HeadImageRender from './scripts/viewManager/utils/headImage';
 
 const drawerWidth = 200;
 
