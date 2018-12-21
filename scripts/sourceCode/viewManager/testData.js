8d8c2fbddb75b5ccf1d95f0452a938761bd9fab8
langyo <langyo.china@gmail.com>
2018-12-22 07:34:25 +0800

    上传测试数据

--- scripts/sourceCode/viewManager/testData.js
+++ scripts/sourceCode/viewManager/testData.js
@@ -2,52 +2,446 @@
 
 export default {
   mainPage: {
-    "headImages": [
+    headImages: [
       {
-        "href": "thread-834159-1-1.html",
-        "img": "http://wx2.sinaimg.cn/large/da4a6115gy1fy7bcqb04nj20hd06mtfb.jpg", "title": "【剪辑】《鱼子的玩家观察手记2》(玩家访谈"
+        href: "834159",
+        img: "http://wx2.sinaimg.cn/large/da4a6115gy1fy7bcqb04nj20hd06mtfb.jpg",
+        title: "【剪辑】《鱼子的玩家观察手记2》(玩家访谈"
       },
       {
-        "href": "thread-829068-1-1.html",
-        "img": "http://attachment.mcbbs.net/forum/201812/09/235920hi6f9ez6vkixq31i.png", "title": "[WIKI][CY] BossShopPro - 插件文档帮助"
+        href: "829068",
+        img: "http://attachment.mcbbs.net/forum/201812/09/235920hi6f9ez6vkixq31i.png",
+        title: "[WIKI][CY] BossShopPro - 插件文档帮助"
       },
       {
-        "href": "thread-828658-1-1.html",
-        "img": "https://raw.githubusercontent.com/Tollainmear/PicRepo/master/%E7%A7%91%E6%8A%80%E7%A9%BA%E5%B2%9B/%E9%A6%96%E9%A1%B5N%E6%A0%BC.jpg", "title": "[1.12.2]卡洛兰科技空岛——终焉之旅"
+        href: "828658",
+        img: "https://raw.githubusercontent.com/Tollainmear/PicRepo/master/%E7%A7%91%E6%8A%80%E7%A9%BA%E5%B2%9B/%E9%A6%96%E9%A1%B5N%E6%A0%BC.jpg",
+        title: "[1.12.2]卡洛兰科技空岛——终焉之旅"
       },
       {
-        "href": "thread-827490-1-1.html",
-        "img": "http://wx2.sinaimg.cn/large/005UHtVDgy1fxi7tl5xbsj32nu0y2npi.jpg", "title": "【史诗】黄金港 | 江灵绝景閟久"
+        href: "827490",
+        img: "http://wx2.sinaimg.cn/large/005UHtVDgy1fxi7tl5xbsj32nu0y2npi.jpg",
+        title: "【史诗】黄金港 | 江灵绝景閟久"
       },
       {
-        "href": "thread-830195-1-1.html",
-        "img": "http://wx2.sinaimg.cn/mw690/8d60bc48gy1fx9fw29jchj20hd06mwk8.jpg", "title": "MineChest-轻巧的ChestCommand配置在线编辑器"
+        href: "830195",
+        img: "http://wx2.sinaimg.cn/mw690/8d60bc48gy1fx9fw29jchj20hd06mwk8.jpg",
+        title: "MineChest-轻巧的ChestCommand配置在线编辑器"
       }
     ],
-    "forumGroups":
-    {
-      "综合讨论":
-      {
-        "forumGroupName": "综合讨论",
-        "forumGroupId": "36",
-        "forums":
-          [
-            {
-              "name": "新闻资讯",
-              "id": "news-1",
-              "avatar": "http://attachment.mcbbs.net/common/e0/common_139_icon.png"
-            },
-            {
-              "name": "游戏技巧",
-              "id": "gameplay-1",
-              "avatar": "http://attachment.mcbbs.net/common/d6/common_39_icon.png"
-            },
-            {
-              "name": "软件资源",
-              "id": "software-1",
-              "avatar": "http://attachment.mcbbs.net/common/17/common_43_icon.png"
-            }, { "name": "编程开发", "id": "development-1", "avatar": "http://attachment.mcbbs.net/common/d1/common_479_icon.png" }, { "name": "翻译&Wiki", "id": "translation-1", "avatar": "http://attachment.mcbbs.net/common/29/common_1015_icon.png" }, { "name": "交易代理", "id": "trade-1", "avatar": "http://attachment.mcbbs.net/common/30/common_647_icon.png" }, { "name": "MCBBS擂台", "id": "arena-1", "avatar": "http://attachment.mcbbs.net/common/13/common_140_icon.png" }]
-      }, "灵感交流": { "forumGroupName": "灵感交流", "forumGroupId": "50", "forums": [{ "name": "矿工茶馆", "id": "chat-1", "avatar": "http://attachment.mcbbs.net/common/9a/common_52_icon.png" }, { "name": "视频实况", "id": "video-1", "avatar": "http://attachment.mcbbs.net/common/42/common_463_icon.png" }, { "name": "材质资源", "id": "texture-1", "avatar": "http://attachment.mcbbs.net/common/f7/common_44_icon.png" }, { "name": "皮肤分享", "id": "skin-1", "avatar": "http://attachment.mcbbs.net/common/36/common_169_icon.png" }, { "name": "周边创作", "id": "accessories-1", "avatar": "http://attachment.mcbbs.net/common/73/common_67_icon.png" }] }, "创世匠艺": { "forumGroupName": "创世匠艺", "forumGroupId": "800", "forums": [{ "name": "展示&共享", "id": "map-1", "avatar": "http://attachment.mcbbs.net/common/39/common_137_icon.png" }, { "name": "搬运&鉴赏", "id": "mapcarry-1", "avatar": "http://attachment.mcbbs.net/common/19/common_801_icon.png" }, { "name": "匠人酒馆", "id": "mapchat-1", "avatar": "http://attachment.mcbbs.net/common/fb/common_1276_icon.png" }] }, "你问我答": { "forumGroupName": "你问我答", "forumGroupId": "364", "forums": [{ "name": "原版问答", "id": "qanda-1", "avatar": "http://attachment.mcbbs.net/common/5f/common_110_icon.png" }, { "name": "联机问答", "id": "multiqanda-1", "avatar": "http://www.mcbbs.net/data/myattachment/common/5f/common_110_icon.png" }, { "name": "Mod问答", "id": "modqanda-1", "avatar": "http://attachment.mcbbs.net/common/f7/common_266_icon.png" }, { "name": "周边问答", "id": "etcqanda-1", "avatar": "http://attachment.mcbbs.net/common/5f/common_110_icon.png" }] }, "Mod(模组)": { "forumGroupName": "Mod(模组)", "forumGroupId": "168", "forums": [{ "name": "Mod讨论", "id": "moddiscussion-1", "avatar": "http://attachment.mcbbs.net/common/ee/common_185_icon.png" }, { "name": "Mod教程", "id": "modtutorial-1", "avatar": "http://attachment.mcbbs.net/common/c7/common_1326_icon.png" }, { "name": "Mod发布", "id": "mod-1", "avatar": "http://attachment.mcbbs.net/common/6c/common_45_icon.png" }, { "name": "整合包发布", "id": "modpack-1", "avatar": "http://attachment.mcbbs.net/common/14/common_170_icon.png" }] }, "多人联机": { "forumGroupName": "多人联机", "forumGroupId": "167", "forums": [{ "name": "联机教程", "id": "multiplayer-1", "avatar": "http://attachment.mcbbs.net/common/8f/common_178_icon.png" }, { "name": "服务器", "id": "server-1", "avatar": "http://attachment.mcbbs.net/common/8f/common_179_icon.png" }, { "name": "服务端插件", "id": "servermod-1", "avatar": "http://attachment.mcbbs.net/common/01/common_138_icon.png" }, { "name": "服务端整合包", "id": "serverpack-1", "avatar": "http://attachment.mcbbs.net/common/38/common_176_icon.png" }, { "name": "【推荐服务器】国家建筑师", "id": "NACW-1", "avatar": "http://attachment.mcbbs.net/common/e5/common_1107_icon.png" }] }, "基岩版": { "forumGroupName": "基岩版", "forumGroupId": "363", "forums": [{ "name": "MCBBS创意馆(基岩版)", "id": "creatorarena-1", "avatar": "http://attachment.mcbbs.net/common/3c/common_499_icon.png" }, { "name": "基岩版技巧教程", "id": "pe-1", "avatar": "http://attachment.mcbbs.net/common/fa/common_164_icon.png" }, { "name": "基岩版软件资源", "id": "peresources-1", "avatar": "http://attachment.mcbbs.net/common/9b/common_365_icon.png" }, { "name": "基岩版作品发布", "id": "pemap-1", "avatar": "http://attachment.mcbbs.net/common/5e/common_366_icon.png" }, { "name": "基岩版多人联机", "id": "pemultiplayer-1", "avatar": "http://attachment.mcbbs.net/common/dd/common_435_icon.png" }, { "name": "Nukkit服务端专区", "id": "nukkit-1", "avatar": "http://attachment.mcbbs.net/common/6e/common_1163_icon.png" }, { "name": "PocketMine服务端专区", "id": "peservermod-1", "avatar": "http://attachment.mcbbs.net/common/fc/common_437_icon.png" }] }, "相关游戏": { "forumGroupName": "相关游戏", "forumGroupId": "104", "forums": [{ "name": "Minecraft: Story Mode (我的世界:故事模式)", "id": "storymode-1", "avatar": "http://attachment.mcbbs.net/common/cd/common_1062_icon.png" }, { "name": "Cobalt", "id": "COBALT-1", "avatar": "http://attachment.mcbbs.net/common/a5/common_1344_icon.png" }, { "name": "Besiege (围攻)", "id": "besiege-1", "avatar": "http://attachment.mcbbs.net/common/1c/common_930_icon.png" }, { "name": "StarBound (星界边境)", "id": "starbound-1", "avatar": "http://attachment.mcbbs.net/common/c3/common_488_icon.png" }, { "name": "Terraria (泰拉瑞亚)", "id": "terraria-1", "avatar": "http://attachment.mcbbs.net/common/3d/common_125_icon.png" }, { "name": "Unturned (未转变者)", "id": "unturned-1", "avatar": "http://attachment.mcbbs.net/common/e7/common_710_icon.png" }, { "name": "Astroneer (异星探险家)", "id": "Astroneer-1", "avatar": "http://attachment.mcbbs.net/common/0e/common_1369_icon.png" }] }, "论坛事务": { "forumGroupName": "论坛事务", "forumGroupId": "1", "forums": [{ "name": "公告和反馈", "id": "announcement-1", "avatar": "http://attachment.mcbbs.net/common/c8/common_2_icon.png" }] }
+    forumGroups: {
+      1: {
+        forumGroupName: "论坛事务",
+        forumGroupId: "1",
+        forums: [
+          {
+            name: "公告和反馈",
+            id: "announcement-1",
+            avatar: "http://attachment.mcbbs.net/common/c8/common_2_icon.png"
+          }
+        ]
+      },
+      36: {
+        forumGroupName: "综合讨论",
+        forumGroupId: "36",
+        forums: [
+          {
+            name: "新闻资讯",
+            id: "news-1",
+            avatar: "http://attachment.mcbbs.net/common/e0/common_139_icon.png"
+          },
+          {
+            name: "游戏技巧",
+            id: "gameplay-1",
+            avatar: "http://attachment.mcbbs.net/common/d6/common_39_icon.png"
+          },
+          {
+            name: "软件资源",
+            id: "software-1",
+            avatar: "http://attachment.mcbbs.net/common/17/common_43_icon.png"
+          },
+          {
+            name: "编程开发",
+            id: "development-1",
+            avatar: "http://attachment.mcbbs.net/common/d1/common_479_icon.png"
+          },
+          {
+            name: "翻译&Wiki",
+            id: "translation-1",
+            avatar: "http://attachment.mcbbs.net/common/29/common_1015_icon.png"
+          },
+          {
+            name: "交易代理",
+            id: "trade-1",
+            avatar: "http://attachment.mcbbs.net/common/30/common_647_icon.png"
+          },
+          {
+            name: "MCBBS擂台",
+            id: "arena-1",
+            avatar: "http://attachment.mcbbs.net/common/13/common_140_icon.png"
+          }
+        ]
+      },
+      50: {
+        forumGroupName: "灵感交流",
+        forumGroupId: "50",
+        forums: [
+          {
+            name: "矿工茶馆",
+            id: "chat-1",
+            avatar: "http://attachment.mcbbs.net/common/9a/common_52_icon.png"
+          },
+          {
+            name: "视频实况",
+            id: "video-1",
+            avatar: "http://attachment.mcbbs.net/common/42/common_463_icon.png"
+          },
+          {
+            name: "材质资源",
+            id: "texture-1",
+            avatar: "http://attachment.mcbbs.net/common/f7/common_44_icon.png"
+          },
+          {
+            name: "皮肤分享",
+            id: "skin-1",
+            avatar: "http://attachment.mcbbs.net/common/36/common_169_icon.png"
+          },
+          {
+            name: "周边创作",
+            id: "accessories-1",
+            avatar: "http://attachment.mcbbs.net/common/73/common_67_icon.png"
+          }
+        ]
+      },
+      104: {
+        forumGroupName: "相关游戏",
+        forumGroupId: "104",
+        forums: [
+          {
+            name: "Minecraft: Story Mode (我的世界:故事模式)",
+            id: "storymode-1",
+            avatar: "http://attachment.mcbbs.net/common/cd/common_1062_icon.png"
+          },
+          {
+            name: "Cobalt",
+            id: "COBALT-1",
+            avatar: "http://attachment.mcbbs.net/common/a5/common_1344_icon.png"
+          },
+          {
+            name: "Besiege (围攻)",
+            id: "besiege-1",
+            avatar: "http://attachment.mcbbs.net/common/1c/common_930_icon.png"
+          },
+          {
+            name: "StarBound (星界边境)",
+            id: "starbound-1",
+            avatar: "http://attachment.mcbbs.net/common/c3/common_488_icon.png"
+          },
+          {
+            name: "Terraria (泰拉瑞亚)",
+            id: "terraria-1",
+            avatar: "http://attachment.mcbbs.net/common/3d/common_125_icon.png"
+          },
+          {
+            name: "Unturned (未转变者)",
+            id: "unturned-1",
+            avatar: "http://attachment.mcbbs.net/common/e7/common_710_icon.png"
+          },
+          {
+            name: "Astroneer (异星探险家)",
+            id: "Astroneer-1",
+            avatar: "http://attachment.mcbbs.net/common/0e/common_1369_icon.png"
+          }
+        ]
+      },
+      167: {
+        forumGroupName: "多人联机",
+        forumGroupId: "167",
+        forums: [
+          {
+            name: "联机教程",
+            id: "multiplayer-1",
+            avatar: "http://attachment.mcbbs.net/common/8f/common_178_icon.png"
+          },
+          {
+            name: "服务器",
+            id: "server-1",
+            avatar: "http://attachment.mcbbs.net/common/8f/common_179_icon.png"
+          },
+          {
+            name: "服务端插件",
+            id: "servermod-1",
+            avatar: "http://attachment.mcbbs.net/common/01/common_138_icon.png"
+          },
+          {
+            name: "服务端整合包",
+            id: "serverpack-1",
+            avatar: "http://attachment.mcbbs.net/common/38/common_176_icon.png"
+          },
+          {
+            name: "【推荐服务器】国家建筑师",
+            id: "NACW-1",
+            avatar: "http://attachment.mcbbs.net/common/e5/common_1107_icon.png"
+          }
+        ]
+      },
+      168: {
+        forumGroupName: "Mod(模组)",
+        forumGroupId: "168",
+        forums: [
+          {
+            name: "Mod讨论",
+            id: "moddiscussion-1",
+            avatar: "http://attachment.mcbbs.net/common/ee/common_185_icon.png"
+          },
+          {
+            name: "Mod教程",
+            id: "modtutorial-1",
+            avatar: "http://attachment.mcbbs.net/common/c7/common_1326_icon.png"
+          },
+          {
+            name: "Mod发布",
+            id: "mod-1",
+            avatar: "http://attachment.mcbbs.net/common/6c/common_45_icon.png"
+          },
+          {
+            name: "整合包发布",
+            id: "modpack-1",
+            avatar: "http://attachment.mcbbs.net/common/14/common_170_icon.png"
+          }
+        ]
+      },
+      363: {
+        forumGroupName: "基岩版",
+        forumGroupId: "363",
+        forums: [
+          {
+            name: "MCBBS创意馆(基岩版)",
+            id: "creatorarena-1",
+            avatar: "http://attachment.mcbbs.net/common/3c/common_499_icon.png"
+          },
+          {
+            name: "基岩版技巧教程",
+            id: "pe-1",
+            avatar: "http://attachment.mcbbs.net/common/fa/common_164_icon.png"
+          },
+          {
+            name: "基岩版软件资源",
+            id: "peresources-1",
+            avatar: "http://attachment.mcbbs.net/common/9b/common_365_icon.png"
+          },
+          {
+            name: "基岩版作品发布",
+            id: "pemap-1",
+            avatar: "http://attachment.mcbbs.net/common/5e/common_366_icon.png"
+          },
+          {
+            name: "基岩版多人联机",
+            id: "pemultiplayer-1",
+            avatar: "http://attachment.mcbbs.net/common/dd/common_435_icon.png"
+          },
+          {
+            name: "Nukkit服务端专区",
+            id: "nukkit-1",
+            avatar: "http://attachment.mcbbs.net/common/6e/common_1163_icon.png"
+          },
+          {
+            name: "PocketMine服务端专区",
+            id: "peservermod-1",
+            avatar: "http://attachment.mcbbs.net/common/fc/common_437_icon.png"
+          }
+        ]
+      },
+      364: {
+        forumGroupName: "你问我答",
+        forumGroupId: "364",
+        forums: [
+          {
+            name: "原版问答",
+            id: "qanda-1",
+            avatar: "http://attachment.mcbbs.net/common/5f/common_110_icon.png"
+          },
+          {
+            name: "联机问答",
+            id: "multiqanda-1",
+            avatar: "http://www.mcbbs.net/data/myattachment/common/5f/common_110_icon.png"
+          },
+          {
+            name: "Mod问答",
+            id: "modqanda-1",
+            avatar: "http://attachment.mcbbs.net/common/f7/common_266_icon.png"
+          },
+          {
+            name: "周边问答",
+            id: "etcqanda-1",
+            avatar: "http://attachment.mcbbs.net/common/5f/common_110_icon.png"
+          }
+        ]
+      },
+      800: {
+        forumGroupName: "创世匠艺",
+        forumGroupId: "800",
+        forums: [
+          {
+            name: "展示&共享",
+            id: "map-1",
+            avatar: "http://attachment.mcbbs.net/common/39/common_137_icon.png"
+          },
+          {
+            name: "搬运&鉴赏",
+            id: "mapcarry-1",
+            avatar: "http://attachment.mcbbs.net/common/19/common_801_icon.png"
+          },
+          {
+            name: "匠人酒馆",
+            id: "mapchat-1",
+            avatar: "http://attachment.mcbbs.net/common/fb/common_1276_icon.png"
+          }
+        ]
+      }
+    },
+    headThreads: {
+      latestThread: [
+        {
+          title: "[原创|单篇|散文][实验性散文]一",
+          href: "835041",
+          author: "495221"
+        },
+        {
+          title: "[1.12.2][MIAX]妈妈再也不怕我用",
+          href: "835038",
+          author: "2371938"
+        },
+        {
+          title: "【圣诞节快乐】圣诞贺皮:)",
+          href: "835037",
+          author: "2495709"
+        },
+        {
+          title: "Minecraft 主机版 TU73/Patch 42",
+          href: "835024",
+          author: "1575538"
+        },
+        {
+          title: "[1.8.8][Spigot]梦幻仙境空岛|连",
+          href: "835012",
+          author: "2465888"
+        },
+        {
+          title: "[综合]Doomsday——末日生存基础",
+          href: "835001",
+          author: "294969"
+        },
+        {
+          title: "神秘时代4物品要素查询工具: 可",
+          href: "834997",
+          author: "72341"
+        },
+        {
+          title: "[1.7.10] [低配可玩][永鸿之日大",
+          href: "834968",
+          author: "2415988"
+        },
+        {
+          title: "如何把IBlockState转为String",
+          href: "834972",
+          author: "2371938"
+        }
+      ],
+      latestReply: [
+        {
+          title: "[BGM]我的世界火风殿魔幻中式地",
+          href: "831189",
+          author: "2231921"
+        },
+        {
+          title: "[WIKI][CY] BossShopPro - 插件",
+          href: "829068",
+          author: "1875841"
+        },
+        {
+          title: "[PCD] 跟着海螺学正则",
+          href: "827651",
+          author: "1196990"
+        },
+        {
+          title: "【史诗】黄金港 | 江灵绝景閟久",
+          href: "827490",
+          author: "518005"
+        },
+        {
+          title: "「Antibiotic 我的抗生素 皮肤合",
+          href: "826784",
+          author: "1286708"
+        },
+        {
+          title: "Minecraft 基岩版 1.7 特性列表",
+          href: "826214",
+          author: "1350847"
+        },
+        {
+          title: "【1.13.x~18w45a | 16x】XeKr的",
+          href: "823957",
+          author: "865498"
+        },
+        {
+          title: "[Wiki][CY/SCT] CMI - 插件文档",
+          href: "823489",
+          author: "1875841"
+        },
+        {
+          title: "[筑龙]现代、欧式建筑 & 4K+1K地",
+          href: "822929",
+          author: "2451704"
+        }
+      ],
+      latestStarThread: [
+        {
+          title: "【发锭】金粒有点多，往外发一发",
+          href: "832735",
+          author: "1810010"
+        },
+        {
+          title: "【联机教程】如何和小伙伴们用最",
+          href: "831469",
+          author: "2227971"
+        },
+        {
+          title: "[BGM] [1.12.2][StoneBlock] -",
+          href: "833239",
+          author: "2610569"
+        },
+        {
+          title: "[mod整合包][1.7.10]科技，冒险",
+          href: "831523",
+          author: "1052423"
+        },
+        {
+          title: "【喵喵喵？】这什么鬼弹射起步？",
+          href: "832142",
+          author: "42863"
+        },
+        {
+          title: "AIE工作室:第二作品发布[1.13.2]",
+          href: "833070",
+          author: "230308"
+        },
+        {
+          title: "【金锭帖】简述来论坛3个月以来",
+          href: "833828",
+          author: "938891"
+        },
+        {
+          title: "[1.12.2][渣机福利][科技空岛]超",
+          href: "832635",
+          author: "2351277"
+        },
+        {
+          title: "【H2O=18w50a】【发锭庆祝】九级",
+          href: "833803",
+          author: "712287"
+        }
+      ]
     }
   },
   forums: {
