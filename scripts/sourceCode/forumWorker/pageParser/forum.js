5c8a67f862c6026daa7c4981648228a8ddcdffd0
langyo <langyo.china@gmail.com>
2018-12-22 07:32:17 +0800

    添加了对头版帖子的解析
    （然而后续将不解析主页的头版帖子，有专用页面）
    （无法正常解析公告版，原因很可能是因为有隐藏版块的无效元素）

--- scripts/sourceCode/forumWorker/pageParser/forum.js
+++ scripts/sourceCode/forumWorker/pageParser/forum.js
@@ -1,11 +1,16 @@
 let mainPage = {
 	headImages: [],
-	forumGroups: {}
+	forumGroups: {},
+	headThreads: {
+		latestThread: [],
+		latestReply: [],
+		latestStarThread: []
+    }
 };
 
 for (let i of document.querySelectorAll('#portal_block_800_content > div.slidebox > div.slideshow > li')) {
 	let n = {
-		href: i.querySelector('a').getAttribute('href'),
+		href: /thread-([0-9]+)-/.exec(i.querySelector('a').getAttribute('href'))[1],
 		img: i.querySelector('a > img').getAttribute('src'),
 		title: i.querySelector('span.title').innerText
 	};
@@ -22,9 +27,9 @@ for (let i of document.querySelectorAll('#ct > div.mn > div.fl.bm > div.bm.bmw.c
 		for (let j of i.querySelectorAll('div.bm_c.forum_index_bm > table > tbody > tr > td')) {
 			try {
 				let n = {
-					name: j.querySelectorAll('dl > dt > a')[0].innerText.trim(),
-					id: /^forum\-([A-Za-z0-9\-]+)\.html$/.exec(j.querySelectorAll('dl > dt > a')[0].getAttribute('href'))[1].trim(),
-					avatar: j.querySelectorAll('div > a > img')[0].getAttribute('src').trim()
+					name: j.querySelector('dl > dt > a').innerText.trim(),
+					id: /^forum\-([A-Za-z0-9\-]+)\.html$/.exec(j.querySelector('dl > dt > a').getAttribute('href'))[1].trim(),
+					avatar: j.querySelector('div > a > img').getAttribute('src').trim()
 				}
 				forums.push(n);
 			} catch (e) {
@@ -37,8 +42,8 @@ for (let i of document.querySelectorAll('#ct > div.mn > div.fl.bm > div.bm.bmw.c
 			try {
 				let n = {
 					name: j.querySelector('td > h2 > a').innerText.trim(),
-					id: /^forum\-([A-Za-z0-9\-]+)\.html$/.exec(j.querySelectorAll('td > h2 > a')[0].getAttribute('href'))[1].trim(),
-					avatar: j.querySelectorAll('td.fl_icn > a > img')[0].getAttribute('src').trim()
+					id: /^forum\-([A-Za-z0-9\-]+)\.html$/.exec(j.querySelector('td > h2 > a').getAttribute('href'))[1].trim(),
+					avatar: j.querySelector('td.fl_icn > a > img').getAttribute('src').trim()
 				};
 				forums.push(n);
 			} catch (e) {
@@ -46,9 +51,30 @@ for (let i of document.querySelectorAll('#ct > div.mn > div.fl.bm > div.bm.bmw.c
 			}
 		}
 	}
-	mainPage.forumGroups[groupName] = {
+	mainPage.forumGroups[groupId] = {
 		forumGroupName: groupName,
 		forumGroupId: groupId,
 		forums: forums
 	};
+}
+for(let i of document.querySelectorAll('#portal_block_807_content > div > ul > li')){
+	mainPage.headThreads.latestThread.push({
+		title: i.querySelectorAll('a')[1].innerText.trim(),
+		href: /thread-([0-9]+)-/.exec(i.querySelectorAll('a')[1].getAttribute('href'))[1],
+		author: /uid=([0-9]+)/.exec(i.querySelector('em > a').getAttribute('href'))[1]
+    });
+}
+for(let i of document.querySelectorAll('#portal_block_806_content > div > ul > li')){
+	mainPage.headThreads.latestReply.push({
+		title: i.querySelectorAll('a')[1].innerText.trim(),
+		href: /thread-([0-9]+)-/.exec(i.querySelectorAll('a')[1].getAttribute('href'))[1],
+		author: /uid=([0-9]+)/.exec(i.querySelector('em > a').getAttribute('href'))[1]
+    });
+}
+for(let i of document.querySelectorAll('#portal_block_809_content > div > ul > li')){
+	mainPage.headThreads.latestStarThread.push({
+		title: i.querySelectorAll('a')[1].innerText.trim(),
+		href: /thread-([0-9]+)-/.exec(i.querySelectorAll('a')[1].getAttribute('href'))[1],
+		author: /uid=([0-9]+)/.exec(i.querySelector('em > a').getAttribute('href'))[1]
+    });
 }
