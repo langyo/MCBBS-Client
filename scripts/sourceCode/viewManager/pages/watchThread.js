27a2e024a6d6efe9ea0d499d301e61908d46f8a3
langyo <langyo.china@gmail.com>
2018-12-24 20:40:47 +0800

    .

--- scripts/sourceCode/viewManager/pages/watchThread.js
+++ scripts/sourceCode/viewManager/pages/watchThread.js
@@ -1,103 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
+import classnames from "classnames";
 
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
+import Floor from "../utils/floor";
 
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
+import testData from "../testData";
 
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+const styles = theme => ({});
 
-import { res } from "../src/lib/resources";
-import TextViewer from "../src/ui/com/textEditor/fullTextViewer";
-
-const style = theme => {
-  root: {
-    width: "100%";
-  }
-};
-
-// 普通楼层
-// Prop 参数：
-//   userId - 此层楼的用户 ID
-//   floorId - 楼层 ID，也就是楼层在 res 里的编号
-//   floorCount - 楼层编号，也就是第几层
-class Floor extends React.Component {
-  handleMoreClick = () => {};
-
+class Thread extends React.Component {
   render() {
     const { classes } = this.props;
 
     return (
-      <Card className={classes.root}>
-        <CardHeader
-          avatar={
-            <Avatar
-              aria-label={res.users[this.props.userId].name}
-              src={res.users.getAvatarUrl(this.props.userId)}
-            />
-          }
-          action={
-            <IconButton>
-              {" "}
-              <MoreVertIcon onClick={this.handleMoreClick} />{" "}
-            </IconButton>
-          }
-          title={res.users[this.props.userId].name}
-          subHeader={res.users.getGroupName(this.props.userId)}
-        />
-        <CardContent>
-          {/* 日期部分 */}
-          <Typography variant="caption" gutterBottom align="center">
-            {"最后编辑于 " + res.floor[this.props.floorId].latestEditTime}
-          </Typography>
-          {/* 内容部分 */}
-          <TextViewer content={res.floor[this.props.floorId].content} />
-          {/* 评分部分 */}
-          <Typography variant="" />
-        </CardContent>
-      </Card>
+      <div>
+        {testData.threads[this.props.thread].posts.map((n, id) => (
+          <Floor
+            key={n}
+            accountName={testData.posts["post_" + n].author}
+            accountInfo={"Lv ? ."}
+            contentTimeInfo={"发布于 ?"}
+            contentFloor={id}
+            content={testData.posts["post_" + n].content}
+            reply
+            rate
+            edit
+          />
+        ))}
+      </div>
     );
   }
 }
 
-Floor.propTypes = {
+Thread.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-// 帖子头部
-// Prop 参数：
-//   threadId - 帖子编号
-class FloorHead extends React.Component {
-  handleMoreClick = () => {};
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <Card className={classes.root}>
-        <CardHeader
-          action={
-            <IconButton>
-              {" "}
-              <MoreVertIcon onClick={this.handleMoreClick} />{" "}
-            </IconButton>
-          }
-          title={res.thread[this.props.threadId].title}
-        />
-        <CardContent> </CardContent>
-      </Card>
-    );
-  }
-}
-
-FloorHead.propTypes = {
-  classes: PropTypes.object.isRequired
-};
+export default withStyles(styles)(Thread);
--- scripts/viewManager/pages/watchThread.js
+++ scripts/viewManager/pages/watchThread.js
@@ -1,115 +1,50 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
 var _react = _interopRequireDefault(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
 var _styles = require("@material-ui/core/styles");
 
-var _Card = _interopRequireDefault(require("@material-ui/core/Card"));
+var _classnames = _interopRequireDefault(require("classnames"));
 
-var _CardHeader = _interopRequireDefault(require("@material-ui/core/CardHeader"));
+var _floor = _interopRequireDefault(require("../utils/floor"));
 
-var _CardMedia = _interopRequireDefault(require("@material-ui/core/CardMedia"));
-
-var _CardContent = _interopRequireDefault(require("@material-ui/core/CardContent"));
-
-var _CardActions = _interopRequireDefault(require("@material-ui/core/CardActions"));
-
-var _Avatar = _interopRequireDefault(require("@material-ui/core/Avatar"));
-
-var _IconButton = _interopRequireDefault(require("@material-ui/core/IconButton"));
-
-var _Typography = _interopRequireDefault(require("@material-ui/core/Typography"));
-
-var _ExpandMore = _interopRequireDefault(require("@material-ui/icons/ExpandMore"));
-
-var _MoreVert = _interopRequireDefault(require("@material-ui/icons/MoreVert"));
-
-var _resources = require("../src/lib/resources");
-
-var _fullTextViewer = _interopRequireDefault(require("../src/ui/com/textEditor/fullTextViewer"));
+var _testData = _interopRequireDefault(require("../testData"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+const styles = theme => ({});
 
-const style = theme => {
-  root: {
-    width: "100%";
-  }
-}; // 普通楼层
-// Prop 参数：
-//   userId - 此层楼的用户 ID
-//   floorId - 楼层 ID，也就是楼层在 res 里的编号
-//   floorCount - 楼层编号，也就是第几层
-
-
-class Floor extends _react.default.Component {
-  constructor(...args) {
-    super(...args);
-
-    _defineProperty(this, "handleMoreClick", () => {});
-  }
-
+class Thread extends _react.default.Component {
   render() {
     const {
       classes
     } = this.props;
-    return _react.default.createElement(_Card.default, {
-      className: classes.root
-    }, _react.default.createElement(_CardHeader.default, {
-      avatar: _react.default.createElement(_Avatar.default, {
-        "aria-label": _resources.res.users[this.props.userId].name,
-        src: _resources.res.users.getAvatarUrl(this.props.userId)
-      }),
-      action: _react.default.createElement(_IconButton.default, null, " ", _react.default.createElement(_MoreVert.default, {
-        onClick: this.handleMoreClick
-      }), " "),
-      title: _resources.res.users[this.props.userId].name,
-      subHeader: _resources.res.users.getGroupName(this.props.userId)
-    }), _react.default.createElement(_CardContent.default, null, _react.default.createElement(_Typography.default, {
-      variant: "caption",
-      gutterBottom: true,
-      align: "center"
-    }, "最后编辑于 " + _resources.res.floor[this.props.floorId].latestEditTime), _react.default.createElement(_fullTextViewer.default, {
-      content: _resources.res.floor[this.props.floorId].content
-    }), _react.default.createElement(_Typography.default, {
-      variant: ""
+    return _react.default.createElement("div", null, _testData.default.threads[this.props.thread].posts.map((n, id) => _react.default.createElement(_floor.default, {
+      key: n,
+      accountName: _testData.default.posts["post_" + n].author,
+      accountInfo: "Lv ? .",
+      contentTimeInfo: "发布于 ?",
+      contentFloor: id,
+      content: _testData.default.posts["post_" + n].content,
+      reply: true,
+      rate: true,
+      edit: true
     })));
   }
 
 }
 
-Floor.propTypes = {
-  classes: _propTypes.default.object.isRequired
-}; // 帖子头部
-// Prop 参数：
-//   threadId - 帖子编号
-
-class FloorHead extends _react.default.Component {
-  constructor(...args) {
-    super(...args);
-
-    _defineProperty(this, "handleMoreClick", () => {});
-  }
-
-  render() {
-    const {
-      classes
-    } = this.props;
-    return _react.default.createElement(_Card.default, {
-      className: classes.root
-    }, _react.default.createElement(_CardHeader.default, {
-      action: _react.default.createElement(_IconButton.default, null, " ", _react.default.createElement(_MoreVert.default, {
-        onClick: this.handleMoreClick
-      }), " "),
-      title: _resources.res.thread[this.props.threadId].title
-    }), _react.default.createElement(_CardContent.default, null, " "));
-  }
-
-}
-
-FloorHead.propTypes = {
+Thread.propTypes = {
   classes: _propTypes.default.object.isRequired
 };
+
+var _default = (0, _styles.withStyles)(styles)(Thread);
+
+exports.default = _default;
