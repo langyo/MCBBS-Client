de872265e928d2198930398b0ba96387458e3a49
langyo <langyo.china@gmail.com>
2018-12-25 18:15:57 +0800

    .

--- scripts/forumWorker/pageParser/userInfo.js
+++ scripts/forumWorker/pageParser/userInfo.js
@@ -1,7 +1,81 @@
 "use strict";
 
-let users = {};
-users[/[0-9]+/.exec(document.querySelectorAll('#ct > div > div.bm.bw0 > div > div.bm_c.u_profile > div:nth-child(1) > h2 > span')[0].innerText.trim())[0]] = {
-  name: document.querySelectorAll('#uhd > div.h.cl > h2')[0].innerText.trim(),
-  group: /gid=([0-9]+)/.exec(document.querySelectorAll('#ct > div > div.bm.bw0 > div > div.bm_c.u_profile > div.pbm.mbm.bbda.cl')[1].querySelectorAll('ul')[0].querySelectorAll('li > span > a')[0].getAttribute('href'))[1]
+let user = {
+  id: /[0-9]+/.exec(document.querySelectorAll('#ct > div > div.bm.bw0 > div > div.bm_c.u_profile > div:nth-child(1) > h2 > span')[0].innerText.trim())[0]
 };
+user.name = document.querySelectorAll('#uhd > div.h.cl > h2')[0].innerText.trim();
+user.avatar = document.querySelector('#uhd > div.h.cl > div > a > img').getAttribute('src').replace('size=small', 'size=big');
+
+for (let i of document.querySelectorAll('div.pbm.mbm.bbda.cl > ul > li')) {
+  switch (i.querySelector('em').innerText.trim()) {
+    case '邮箱状态':
+      i.removeChild(i.querySelector('em'));
+      user.mailState = i.innerText.trim();
+      break;
+
+    case '视频认证':
+      i.removeChild(i.querySelector('em'));
+      user.videoProve = i.innerText.trim();
+      break;
+
+    case '自定义头衔':
+      i.removeChild(i.querySelector('em'));
+      user.rank = i.innerText.trim();
+      break;
+
+    case '个人签名':
+      i.removeChild(i.querySelector('em'));
+      user.signature = i.innerHTML;
+      break;
+    // case '统计信息':
+
+    case '星座':
+      i.removeChild(i.querySelector('em'));
+      user.starSign = i.innerText.trim();
+      break;
+
+    case '个人主页':
+      i.removeChild(i.querySelector('em'));
+      user.homePage = i.innerText.trim();
+      break;
+
+    case 'Java版ID':
+      i.removeChild(i.querySelector('em'));
+      user.javaVersionID = i.innerText.trim();
+      break;
+
+    case '基岩版ID':
+      i.removeChild(i.querySelector('em'));
+      user.bedrockVersionID = i.innerText.trim();
+      break;
+    // case '管理组':
+    // case '用户组':
+    // case '扩展用户组':
+
+    case '在线时间':
+      i.removeChild(i.querySelector('em'));
+      user.onlineTimeCount = i.innerText.trim();
+      break;
+
+    case '注册时间':
+      i.removeChild(i.querySelector('em'));
+      user.registerTime = i.innerText.trim();
+      break;
+
+    case '最后访问':
+    case '上次活动时间':
+      i.removeChild(i.querySelector('em'));
+      user.latestOnlineTime = i.innerText.trim();
+      break;
+
+    case '上次发表时间':
+      i.removeChild(i.querySelector('em'));
+      user.latestReplyTime = i.innerText.trim();
+      break;
+
+    case '所在时区':
+      i.removeChild(i.querySelector('em'));
+      user.timeZone = i.innerText.trim();
+      break;
+  }
+}
