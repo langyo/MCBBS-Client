61b08d38f2d02fb9faa1d7781dba9339f0f1823c
langyo <langyo.china@gmail.com>
2018-12-26 11:08:12 +0800

    添加了一些测试数据
    （不过量倒是挺大的）

--- scripts/forumWorker/pageParser/userInfo.js
+++ scripts/forumWorker/pageParser/userInfo.js
@@ -8,100 +8,104 @@ user.name = document.querySelectorAll('#uhd > div.h.cl > h2')[0].innerText.trim(
 user.avatar = document.querySelector('#uhd > div.h.cl > div > a > img').getAttribute('src').replace('size=small', 'size=big');
 
 for (let i of document.querySelectorAll('div.pbm.mbm.bbda.cl > ul > li')) {
-  switch (i.querySelector('em').innerText.trim()) {
-    case '邮箱状态':
-      i.removeChild(i.querySelector('em'));
-      user.mailState = i.innerText.trim();
-      break;
+  try {
+    switch (i.querySelector('em').innerText.trim()) {
+      case '邮箱状态':
+        i.removeChild(i.querySelector('em'));
+        user.mailState = i.innerText.trim();
+        break;
 
-    case '视频认证':
-      i.removeChild(i.querySelector('em'));
-      user.videoProve = i.innerText.trim();
-      break;
+      case '视频认证':
+        i.removeChild(i.querySelector('em'));
+        user.videoProve = i.innerText.trim();
+        break;
 
-    case '自定义头衔':
-      i.removeChild(i.querySelector('em'));
-      user.rank = i.innerText.trim();
-      break;
+      case '自定义头衔':
+        i.removeChild(i.querySelector('em'));
+        user.rank = i.innerText.trim();
+        break;
 
-    case '个人签名':
-      i.removeChild(i.querySelector('em'));
-      user.signature = i.innerHTML;
-      break;
+      case '个人签名':
+        i.removeChild(i.querySelector('em'));
+        user.signature = i.innerHTML;
+        break;
 
-    case '统计信息':
-      for (let j of i.querySelectorAll('a')) {
-        let match = /^([^0-9\s]+)\s*([0-9]+)$/.exec(j.innerText);
+      case '统计信息':
+        for (let j of i.querySelectorAll('a')) {
+          let match = /^([^0-9\s]+)\s*([0-9]+)$/.exec(j.innerText);
 
-        switch (match[1]) {
-          case '好友数':
-            user.states.friendCount = match[2];
-            break;
+          switch (match[1]) {
+            case '好友数':
+              user.states.friendCount = match[2];
+              break;
 
-          case '回帖数':
-            user.states.replyCount = match[2];
-            break;
+            case '回帖数':
+              user.states.replyCount = match[2];
+              break;
 
-          case '主题数':
-            user.states.threadCount = match[2];
-            break;
+            case '主题数':
+              user.states.threadCount = match[2];
+              break;
+          }
         }
-      }
 
-    case '星座':
-      i.removeChild(i.querySelector('em'));
-      user.starSign = i.innerText.trim();
-      break;
+      case '星座':
+        i.removeChild(i.querySelector('em'));
+        user.starSign = i.innerText.trim();
+        break;
 
-    case '个人主页':
-      i.removeChild(i.querySelector('em'));
-      user.homePage = i.innerText.trim();
-      break;
+      case '个人主页':
+        i.removeChild(i.querySelector('em'));
+        user.homePage = i.innerText.trim();
+        break;
 
-    case 'Java版ID':
-      i.removeChild(i.querySelector('em'));
-      user.javaVersionID = i.innerText.trim();
-      break;
+      case 'Java版ID':
+        i.removeChild(i.querySelector('em'));
+        user.javaVersionID = i.innerText.trim();
+        break;
 
-    case '基岩版ID':
-      i.removeChild(i.querySelector('em'));
-      user.bedrockVersionID = i.innerText.trim();
-      break;
+      case '基岩版ID':
+        i.removeChild(i.querySelector('em'));
+        user.bedrockVersionID = i.innerText.trim();
+        break;
 
-    case '管理组':
-      user.managerGroup = /gid=([0-9]+)/.exec(i.querySelector('a').getAttribute('href'))[1];
-      break;
+      case '管理组':
+        user.managerGroup = /gid=([0-9]+)/.exec(i.querySelector('a').getAttribute('href'))[1];
+        break;
 
-    case '用户组':
-      user.userGroup = /gid=([0-9]+)/.exec(i.querySelector('a').getAttribute('href'))[1];
-      break;
-    // case '扩展用户组':
+      case '用户组':
+        user.userGroup = /gid=([0-9]+)/.exec(i.querySelector('a').getAttribute('href'))[1];
+        break;
+      // case '扩展用户组':
 
-    case '在线时间':
-      i.removeChild(i.querySelector('em'));
-      user.onlineTimeCount = i.innerText.trim();
-      break;
+      case '在线时间':
+        i.removeChild(i.querySelector('em'));
+        user.onlineTimeCount = i.innerText.trim();
+        break;
 
-    case '注册时间':
-      i.removeChild(i.querySelector('em'));
-      user.registerTime = i.innerText.trim();
-      break;
+      case '注册时间':
+        i.removeChild(i.querySelector('em'));
+        user.registerTime = i.innerText.trim();
+        break;
 
-    case '最后访问':
-    case '上次活动时间':
-      i.removeChild(i.querySelector('em'));
-      user.latestOnlineTime = i.innerText.trim();
-      break;
+      case '最后访问':
+      case '上次活动时间':
+        i.removeChild(i.querySelector('em'));
+        user.latestOnlineTime = i.innerText.trim();
+        break;
 
-    case '上次发表时间':
-      i.removeChild(i.querySelector('em'));
-      user.latestReplyTime = i.innerText.trim();
-      break;
+      case '上次发表时间':
+        i.removeChild(i.querySelector('em'));
+        user.latestReplyTime = i.innerText.trim();
+        break;
 
-    case '所在时区':
-      i.removeChild(i.querySelector('em'));
-      user.timeZone = i.innerText.trim();
-      break;
+      case '所在时区':
+        i.removeChild(i.querySelector('em'));
+        user.timeZone = i.innerText.trim();
+        break;
+    }
+  } catch (e) {
+    console.log(e);
   }
 }
 
