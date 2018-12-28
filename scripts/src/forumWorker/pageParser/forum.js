aa177dd71d4a6cb0080709ff798a8533e4dab382
langyo <langyo.china@gmail.com>
2018-12-28 21:26:15 +0800

    .

--- scripts/src/forumWorker/pageParser/forum.js
+++ scripts/src/forumWorker/pageParser/forum.js
@@ -3,7 +3,12 @@ let forum = {
 	threads: []
 };
 
+let threads = [];
+
 for(let i of document.querySelectorAll('#threadlisttableid > tbody > tr')){
 	if(i.className !== "") continue;
 	forum.threads.push(/thread-([0-9]+)-/.exec(i.querySelector('th > a.s.xst').getAttribute('href'))[1]);
+	threads.push({
+		author: i.querySelector('td:nth-child(3) > cite > a') && /uid=([0-9]+)/.exec(i.querySelector('td:nth-child(3) > cite > a').getAttribute('href'))[1] || "0"
+	});
 }
