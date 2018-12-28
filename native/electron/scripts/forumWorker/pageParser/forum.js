b2174cee95903ca73f78cf864c6f47aa5ce7dffd
langyo <langyo.china@gmail.com>
2018-12-28 21:48:04 +0800

    .

--- native/electron/scripts/forumWorker/pageParser/forum.js
+++ native/electron/scripts/forumWorker/pageParser/forum.js
@@ -10,6 +10,9 @@ for (let i of document.querySelectorAll('#threadlisttableid > tbody > tr')) {
   if (i.className !== "") continue;
   forum.threads.push(/thread-([0-9]+)-/.exec(i.querySelector('th > a.s.xst').getAttribute('href'))[1]);
   threads.push({
-    author: i.querySelector('td:nth-child(3) > cite > a') && /uid=([0-9]+)/.exec(i.querySelector('td:nth-child(3) > cite > a').getAttribute('href'))[1] || "0"
+    author: i.querySelector('td:nth-child(3) > cite > a') && /uid=([0-9]+)/.exec(i.querySelector('td:nth-child(3) > cite > a').getAttribute('href'))[1] || "0",
+    title: i.querySelector('th > a.s.xst').innerText.trim(),
+    type: i.querySelector('th > em > a') && /typeid=([0-9]+)/.exec(i.querySelector('th > em > a').getAttribute('href'))[1] || "0",
+    states: {}
   });
 }
