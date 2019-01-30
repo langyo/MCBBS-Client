933615235b367431c6249d7eddd3ce850810e43a
langyo <langyo.china@gmail.com>
2019-01-30 16:07:50 +0800

    修改了 pageBindScript 的数据结构
    P.S. 这玩意……真的好难搞……

--- native/electron/localScripts/localWebView/webviewRender.js
+++ native/electron/localScripts/localWebView/webviewRender.js
@@ -82,27 +82,42 @@ class VirtualBrowser extends React.Component {
     /**
      * @description 用于根据一个具体的 url 或标识对象，创建一个新的 webview
      * @param {string|object} URL 或 一个包含了一些标识信息的对象
-     * @todo 准备支持传入对象（不过 URL 形式也保留）
-     * @todo 另外额外提一句，这个包含了额外标识信息的对象，其中必定有 type 与 url 两个键（也就是说不论你以哪种方式传入参数，此子程序总会自动帮你补全 url）
+     * @example 下面是传入对象的基本数据结构；这种数据结构不仅仅用于创建浏览器，也用于所虚拟浏览器对象的相关说明信息存储
+     * {
+     *   type: "thread",  // 要查看有哪些类型，请查阅 scripts/src/forumWorker/pageBindScript
+     *   url: "http://www.mcbbs.net/thread-811478-1-1.html",  // 如果传入的对象没有 URL，newBrowser 函数会自动生成一个的呢
+     *   args: {
+     *     // 这里生成的供读取的链接参数，依赖于 scripts/src/forumWorker/pageBindScript
+     *     threadID: 811478,
+     *     threadPageIndex: 1
+     *   }
+     * }
      */
     newBrowser = (n) => {
-        for (let i of Object.keys(pageBindScript)) {
-            for (let exprString of pageBindScript[i].url) {
-                // 如果匹配对应正则表达式，则凭此项对应的 preload 列表对 <webview /> 进行初始化
-                let expr = new RegExp(exprString);
-                if (expr.test(n)) {
-                    console.log('%cMainThread ', 'color: blue;', "成功匹配 " + n);
-                    this.taskStack.push(n);
-                    console.log(this.taskStack);
-                    this.checkBrowserStack();
-                    this.hasLoad && this.setState({});
-                    console.log(this.virtualBrowsers);
-                    return;
+        if(typeof n === 'object'){
+            // 给定参数为对象时，直接解析
+
+        }else if(typeof n === 'string'){
+            // 给定参数为字符串时，当作 URL 进行解析
+            for (let i of Object.keys(pageBindScript)) {
+                for (let exprString of pageBindScript[i].url) {
+                    // 如果匹配对应正则表达式，则凭此项对应的 preload 列表对 <webview /> 进行初始化
+                    let expr = new RegExp(exprString);
+                    if (expr.test(n)) {
+                        console.log('%cMainThread ', 'color: blue;', "成功匹配 " + n);
+                        /** @todo 这里 push 的东西将不会是字符串了，而是虚拟浏览器的相关信息说明对象 */
+                        this.taskStack.push(n);
+                        console.log(this.taskStack);
+                        this.checkBrowserStack();
+                        this.hasLoad && this.setState({});
+                        console.log(this.virtualBrowsers);
+                        return;
+                    }
                 }
             }
-        }
-        // 没有任何匹配时，当然就要报错了
-        throw Error("URL 解析错误：已阅，狗屁不通！");
+            // 没有任何匹配时，当然就要报错了
+            throw Error("[参数错误] URL 解析错误：已阅，狗屁不通！");
+        }else throw Error("[参数错误] 你到底传进来个啥玩意嘛……")
     }
 
     render() {
