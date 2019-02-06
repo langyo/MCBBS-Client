f02b664eb91a748dc77aa8c113f345631ca81b80
langyo <m13776491897@163.com>
2019-02-06 10:38:45 +0800

    更改了客户端在 Android 下的应用名

--- native/reactNative/android/app/src/main/java/com/mcbbsclient/MainActivity.java
+++ native/reactNative/android/app/src/main/java/com/mcbbsclient/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "MCBBSClient";
+        return "MCBBS 论坛客户端";
     }
 }
