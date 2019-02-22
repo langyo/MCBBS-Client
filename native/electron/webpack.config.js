1c828fae311dc6c6504eee7f83220869dfebada0
simon3000 <simon3000@163.com>
2019-02-23 01:54:14 +0800

    babel对react支持

--- native/electron/webpack.config.js
+++ native/electron/webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
       use: {
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env']
+          presets: ['@babel/preset-react' ,'@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-class-properties']
         }
       }
     }]
