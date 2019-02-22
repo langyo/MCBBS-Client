b509e7691a70647d37e3d523c86ed002ca2ece25
simon3000 <simon3000@163.com>
2019-02-23 01:33:27 +0800

    添加Webpack

---
+++ native/electron/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  entry: './localScripts/localViewManager/mainViewRender.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  }
+}
