55ceba2f447ba51ef82e0e149f92935260807a1f
simon3000 <simon3000@163.com>
2019-02-24 05:23:49 +0800

    Revert "react-hot-reloader"
    This reverts commit 20a37a2d55928d27a6590f97b6ba28584ccc947e.

--- native/electron/babel.config.js
+++ native/electron/babel.config.js
@@ -2,10 +2,7 @@ module.exports = api => {
   api.cache(true)
 
   const presets = ['@babel/preset-react', '@babel/preset-env']
-  const plugins = [
-    '@babel/plugin-proposal-class-properties',
-    'react-hot-loader/babel'
-  ]
+  const plugins = ['@babel/plugin-proposal-class-properties']
 
   return {
     presets,
