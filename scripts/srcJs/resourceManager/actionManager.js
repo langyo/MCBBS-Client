bafb33c763d233a5420dc0a298519269ab752d45
langyo <langyo.china@gmail.com>
2019-03-03 09:16:11 +0800

    完善了 actionManager

--- scripts/srcJs/resourceManager/actionManager.js
+++ scripts/srcJs/resourceManager/actionManager.js
@@ -1,6 +1,7 @@
+import Reflux from "reflux";
 import Actions from "./actions";
 
-let actions = [];
+let actions = {};
 
 /**
  * @description 用于为不是全局的 Store 创建 Actions
@@ -9,7 +10,10 @@ let actions = [];
  * @returns {Reflux.Actions} 返回为此 Store 创建的 Actions
  */
 export function createActions(source, id){
-    
+    if(Actions.single[source] === undefined) throw new Error("无法为名称为 " + source + " 的 Store 对象创建独立 Actions");
+    if(actions[source] === undefined) actions[source] = {};
+    actions[source][id] = Reflux.createActions(Actions.single[source]);
+    return actions[source][id];
 }
 
 /**
@@ -19,5 +23,6 @@ export function createActions(source, id){
  * @returns {Reflux.Actions} 返回为此 Store 创建的 Actions
  */
 export function getActions(source, id){
-
+    if(actions[source] === undefined) actions[source] = {};
+    return actions[source][id];
 }
