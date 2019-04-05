995a58af5d4aa6d623b5d7a1a834e8b867ffc709
InitAuther97 <1602375324@qq.com>
2019-04-05 18:15:00 +0800

    WTF??????

--- env/java/src/net.mcbbs.client/fixer/Launcher.java
+++
@@ -1,7 +0,0 @@
-package net.mcbbs.client.fixer;
-
-public class Launcher {
-    public static void main(String[] args){
-        //ProcessBuilder pBuilder = new ProcessBuilder("nodejs")
-    }
-}
--- env/java/src/net.mcbbs.client/installer/Installer.java
+++
@@ -1,4 +0,0 @@
-package net.mcbbs.client.installer;
-
-public class Installer extends {
-}
