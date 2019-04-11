5a4485b50ca7e51767db7fc2adf03fce5882fd09
langyo <langyo.china@gmail.com>
2019-04-11 12:09:09 +0800

    添加了 Node 与 Java 通讯的测试程序

---
+++ demos/node-telnet-test/test.js
@@ -0,0 +1,18 @@
+let net = require("net");
+
+let client = net.connect(9233, 'localhost');
+
+client.setEncoding('utf-8');
+
+client.on('connect', () => {
+    client.write('TestC\r\n');
+    console.log("Get!");
+});
+
+client.on('data', conn => {
+    console.log(conn.toString());
+})
+
+client.on('end', () => {
+    console.log("Done!");
+})
