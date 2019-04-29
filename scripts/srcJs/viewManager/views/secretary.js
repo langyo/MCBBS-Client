7363edcc4b8d143838c8627eed76b2bbd56040dc
langyo <langyo.china@gmail.com>
2019-04-29 18:05:57 +0800

    模型更换测试 （换成了血小板）

--- scripts/srcJs/viewManager/views/secretary.js
+++ scripts/srcJs/viewManager/views/secretary.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class Secretary extends React.Component {
     componentDidMount() {
-        loadlive2d("live2d", "./live2d/model/tia/model.json");
+        loadlive2d("live2d", "./live2d/model/blood_platelet/model.json");
     }
 
     render() {
