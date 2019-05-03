1be6b14c544e2acaa655f25009dcf55cb6a36dec
langyo <m13776491897@163.com>
2019-05-04 00:01:53 +0800

    初始化 Android 本地项目

---
+++ native/androidNative/app/src/main/java/net/mcbbs/client/android/MainActivity.java
@@ -0,0 +1,17 @@
+package net.mcbbs.client.android;
+
+import android.app.*;
+import android.os.*;
+import android.webkit.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		
+		
+    }
+}
