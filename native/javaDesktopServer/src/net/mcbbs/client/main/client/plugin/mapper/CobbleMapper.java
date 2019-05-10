c8354bfe4da69c292ab67a578c98fc230092730c
InitAuther97 <1602375324@qq.com>
2019-05-10 21:53:53 +0800

    Added copyright descriptions

--- native/javaDesktopServer/src/net/mcbbs/client/main/client/plugin/mapper/CobbleMapper.java
+++ native/javaDesktopServer/src/net/mcbbs/client/main/client/plugin/mapper/CobbleMapper.java
@@ -1,3 +1,19 @@
+/*
+   Copyright 2019 langyo<langyo.china@gmail.com> and contributors
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
 package net.mcbbs.client.main.client.plugin.mapper;
 
 import com.google.common.collect.Maps;
@@ -10,15 +26,15 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
-import java.util.List;
+//import java.util.Arrays;
+//import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 
 public class CobbleMapper<I extends IPlugin, T> implements Mapper<T> {
 
-    private static final List<String> METHOD_WITHOUT_CHECKING_INSTANCE = Arrays.asList("setInvocationHandlerFactory");
+    //private static final List<String> METHOD_WITHOUT_CHECKING_INSTANCE = Arrays.asList("setInvocationHandlerFactory");
 
     protected final Map<Method, Method> method_mapping = Maps.newHashMap();
     protected final Map<Method, Function<Object[], Object[]>> arg_mapping = Maps.newHashMap();
@@ -38,7 +54,7 @@ public class CobbleMapper<I extends IPlugin, T> implements Mapper<T> {
                     boolean instanceSet = false;
                     @Override
                     public String before(Object instance, Method method, Object[] args) {
-                        if (method.getName().equals("setInvocationHandlerFactory")){
+                        if (METHOD_WITHOUT_CHECKING_INSTANCE.contains(method.getName())){
                             if(args[0]!=null)instanceSet=true;
                             else instanceSet=false;
                         }
