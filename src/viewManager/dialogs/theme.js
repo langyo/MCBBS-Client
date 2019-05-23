0198184f906cdd70e47ba5f5e8de6c778e917883
langyo <langyo.china@gmail.com>
2019-05-24 06:58:00 +0800

    弃用 react-color 库，自行创建颜色选择列表

--- src/viewManager/dialogs/theme.js
+++ src/viewManager/dialogs/theme.js
@@ -19,8 +19,10 @@ import Button from '@material-ui/core/Button';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-
-import { CirclePicker } from 'react-color';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
 
 import Stores from '../../resourceManager/stores';
 import Actions from "../../resourceManager/actions";
@@ -54,23 +56,87 @@ class MainWindowManager extends Reflux.Component {
                             <Typography variant="subtitle1" className={classes.subTitle}>
                                 主色
                             </Typography>
-                            <CirclePicker
-                                width="300px"
-                                colors={['#39C5BB', '#FFA500', '#FFE211', '#FAAFBE', '#66CCFF', '#99FFFF', '#EE0000']}
-                                onChange={(color, event) => Actions.view.global.theme.togglePrimary(color.hex)}
-                            />
+                            <List>
+                            {
+                                [
+                                    {
+                                        color: '#39C5BB',
+                                        name: "初音绿"
+                                    },
+                                    {
+                                        color: '#FFA500',
+                                        name: "镜音铃橙"
+                                    },
+                                    {
+                                        color: '#FFE211',
+                                        name: "镜音连黄"
+                                    },
+                                    {
+                                        color: '#FAAFBE',
+                                        name: "巡音粉"
+                                    },
+                                    {
+                                        color: '#66CCFF',
+                                        name: "天依蓝"
+                                    },
+                                    {
+                                        color: '#99FFFF',
+                                        name: "言和青"
+                                    },
+                                    {
+                                        color: '#EE0000',
+                                        name: "正绫红"
+                                    }].map(n => (
+                                    <ListItem button onClick={() => Actions.view.global.theme.togglePrimary(n.color)} key={n.color}>
+                                        <Avatar style={{backgroundColor: n.color}} />
+                                        <ListItemText primary={n.name} secondary={this.state.primaryColor == n.color ? "已选中" : ""}/>
+                                    </ListItem>
+                                ))
+                            }
+                            </List>
                         </CardContent>
-                    </Card>
-                    <Card>
                         <CardContent>
                             <Typography variant="subtitle1" className={classes.subTitle}>
                                 副色
                             </Typography>
-                            <CirclePicker
-                                width="300px"
-                                colors={['#39C5BB', '#FFA500', '#FFE211', '#FAAFBE', '#66CCFF', '#99FFFF', '#EE0000']}
-                                onChange={(color, event) => Actions.view.global.theme.toggleSecondary(color.hex)}
-                            />
+                            <List>
+                            {
+                                [
+                                    {
+                                        color: '#39C5BB',
+                                        name: "初音绿"
+                                    },
+                                    {
+                                        color: '#FFA500',
+                                        name: "镜音铃橙"
+                                    },
+                                    {
+                                        color: '#FFE211',
+                                        name: "镜音连黄"
+                                    },
+                                    {
+                                        color: '#FAAFBE',
+                                        name: "巡音粉"
+                                    },
+                                    {
+                                        color: '#66CCFF',
+                                        name: "天依蓝"
+                                    },
+                                    {
+                                        color: '#99FFFF',
+                                        name: "言和青"
+                                    },
+                                    {
+                                        color: '#EE0000',
+                                        name: "正绫红"
+                                    }].map(n => (
+                                    <ListItem button onClick={() => Actions.view.global.theme.toggleSecondary(n.color)} key={n.color}>
+                                        <Avatar style={{backgroundColor: n.color}} />
+                                        <ListItemText primary={n.name} secondary={this.state.secondaryColor == n.color ? "已选中" : ""}/>
+                                    </ListItem>
+                                ))
+                            }
+                            </List>
                         </CardContent>
                     </Card>
                 </DialogContent>
