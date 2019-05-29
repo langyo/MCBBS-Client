62d5071a7cf2470c35b858b07a3863533a1c7fea
langyo <langyo.china@gmail.com>
2019-05-29 11:35:35 +0800

    初始化完整的 cs 项目（可能含其它临时文件）

---
+++ native/desktop/csServer/MCBBS Client CS Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MCBBS_Client_CS_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
